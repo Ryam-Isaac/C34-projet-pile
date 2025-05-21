36cc4f97c481a84ac7ec4438b6a4ae095c8fcfcc
Ryam_Isaac <ryamisaac1@gmail.com>
2025-05-21 13:34:25 -0400

    Update ClasseExerciceStack.java

--- ClasseExerciceStack.java
+++ ClasseExerciceStack.java
@@ -4,35 +4,66 @@ import java.util.Stack;
 
 public class ClasseExerciceStack {
 
-    public static void main(String[] args)
-    {
-        // créer une pile
+    public static void main(String[] args) {
 
-        //1 ajouter une cravate de code 718 dans la pile
+        // 1. Créer une pile générique (Object)
+        Stack<Object> pile = new Stack<>();
 
-        //2 ajouter une cravate de code 22 dans la pile
+        // 1 ajouter une cravate de code "718" dans la pile
+        pile.push(new Cravate("718"));
 
-        //3 ajouter un maillot de code 123 et de grandeur moyenne
+        // 2 ajouter une cravate de code "22" dans la pile
+        pile.push(new Cravate("22"));
 
-        //4 afficher le nombre d'objets dans la pile à l'aide d'une méthode
+        // 3 ajouter un maillot de code "123" et taille "M"
+        pile.push(new Maillot("123", "M"));
 
-        //5 créez une deuxième pile identique à la première et contenant les mêmes articles, peut-on faire ça en une ligne de code ?
+        // 4 afficher le nombre d'objets dans la pile
+        System.out.println("Nombre d'objets dans la pile : " + pile.size());
 
-        //6 en les retirant un à un de la pile originale, calculer le total des prix des articles contenus dans la pile
+        // 5 créer une deuxième pile identique à la première (clone)
+        @SuppressWarnings("unchecked")
+        Stack<Object> pile2 = (Stack<Object>) pile.clone();
 
-        //7 Créez maintenant une Pile qui ne pourra contenir que des Articles
+        // 6 retirer un par un les objets de la pile originale et calculer total des prix
+        double totalPrix = 0;
+        while (!pile.isEmpty()) {
+            Object obj = pile.pop();
+            if (obj instanceof Article) {
+                totalPrix += ((Article) obj).getPrix();
+            }
+        }
+        System.out.println("Total prix retirés de la pile originale : " + totalPrix);
 
-        //8 ajouter un objet Cravate, code 565 à cette pile, est-ce possible ?
+        // 7 créer une pile ne contenant que des Articles
+        Stack<Article> pileArticles = new Stack<>();
 
-        //9 ajouter un objet String à cette pile , est-ce possible ?
+        // 8 ajouter un objet Cravate code "565" à cette pile
+        pileArticles.push(new Cravate("565"));  // OK
 
-        //10 ajouter deux autres cravates à cette pile, code 456 et 242
+        // 9 essayer d'ajouter un String (décommente pour voir erreur)
+//      pileArticles.push("Une chaîne");  // NON, compile error
 
-        //11 en les retirant un à un de cette pile , calculer le total des prix des articles contenus dans cette pile
+        // 10 ajouter deux autres cravates code "456" et "242"
+        pileArticles.push(new Cravate("456"));
+        pileArticles.push(new Cravate("242"));
 
-        //12 en utilisant la méthode search, retrouver l'ordre de sortie (1 étant le dessus de la pile) de la cravate avec le code 22 dans la deuxième pile.
-        //La méthode equals de Cravate doit être définie. Voir https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html 
-        //pour comprendre quel est l'argument qui doit être passé à search.
-        //Refaire avec une cravate ayant un code qui ne figure pas dans celles de la pile.
+        // 11 retirer un par un et calculer total des prix
+        double totalPrix2 = 0;
+        while (!pileArticles.isEmpty()) {
+            Article art = pileArticles.pop();
+            totalPrix2 += art.getPrix();
+        }
+        System.out.println("Total prix retirés de la pile Articles : " + totalPrix2);
+
+        // 12 utiliser search pour trouver la position de la cravate "22" dans la pile2
+        Cravate rechercheCravate22 = new Cravate("22");
+        int position22 = pile2.search(rechercheCravate22);
+        System.out.println("Position cravate '22' dans pile2 : " + position22);
+
+        // Rechercher cravate absente ("999")
+        Cravate rechercheCravate999 = new Cravate("999");
+        int position999 = pile2.search(rechercheCravate999);
+        System.out.println("Position cravate '999' dans pile2 (absente) : " + position999);
     }
 }
